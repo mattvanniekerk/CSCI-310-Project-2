5c0e2beba1abb0f9fd736bbc51a598006bc3ec03
Matthew van Niekerk <vannieke@usc.edu>
2017-04-21 21:05:12 -0700

    Copied PaperClass.php to class.paper.php; edited CreateDB.php; modified format of paper text files

---
+++ class.paper.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<?php
+	
+	class Paper {
+		private $title;
+		private $authors = [];
+		private $conference;
+		private $abstract;
+		private $content;
+		private $link;
+		private $bibtex;
+
+		public function getTitle(){
+			return $this->title;
+		}
+
+		public function setTitle($title){
+			$this->title = $title;
+		}
+
+		public function getAuthors(){
+			return $this->authors;
+		}
+
+		public function setAuthors($authors){
+			$this->authors = $authors;
+		}
+
+		public function getConference(){
+			return $this->conference;
+		}
+
+		public function setConference($conference){
+			$this->conference = $conference;
+		}
+
+		public function getAbstract(){
+			return $this->abstract;
+		}
+
+		public function setAbstract($abstract){
+			$this->abstract = $abstract;
+		}
+
+		public function getContent(){
+			return $this->content;
+		}
+
+		public function setContent($content){
+			$this->content = $content;
+		}
+
+		public function getLink(){
+			return $this->link;
+		}
+
+		public function setLink($link){
+			$this->link = $link;
+		}
+
+		public function getBibtex(){
+			return $this->bibtex;
+		}
+
+		public function setBibtex($bibtex){
+			$this->bibtex = $bibtex;
+		}
+	}
+
+?>
+</body>
+</html>
--- database/CreateDB.php
+++ database/CreateDB.php
@@ -1,7 +1,13 @@
 <?php
 	
-	
+	require_once('../class.paper.php');
+
 	$papers = [];
+	$file;
 
-
+	for ($i = 1; $i <= 10; $i++) {
+		$file = file_get_contents("database/papers/paper".$i.".txt");
+		$papers[$i] = new Paper();
+		echo $file;
+	}
 ?>
