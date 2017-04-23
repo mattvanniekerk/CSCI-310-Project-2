eaf087ea7e0766769d447bc4dc3953c8e60cbfc1
Arman Akhbari <akhbari@usc.edu>
2017-04-22 20:22:26 -0700

    Arman commit paperListPage

--- pages/paper_list.php
+++ pages/paper_list.php
@@ -52,23 +52,118 @@ $frequencies = [];
         <th id = "paperDownloadLink" style="padding:0 15px 0 15px;"> Download Link</th>
         <th id = "paperBibtext" style="padding:0 15px 0 15px;"> Bibtext</th>
 
+
     </tr>
-    
-        <?php for ($i=1; $i <= 10; $i++) { ?>
-            <tr> 
-                <td> <?php echo $frequencies[$i]?> </td>
-                <td> <?php echo $papers[$i]->getTitle();?> </td>
-                <td> <?php echo $papers[$i]->getAuthors(); ?> </td>
-                <td> <?php echo $papers[$i]->getConference();?> </td>
-                <td> <a href=" https://www.youtube.com/">  <?php echo $papers[$i]->getLink();?></a> </td>
-                <td> <?php echo $papers[$i]->getBibtex(); ?></td>
+    <script>
 
-            </tr>
-         <?php } ?>
-   
+function Paper(){
+    this.title = "";
+    this.authors = [];
+    this.conference = "";
+    this.link = "";
+    this.abstract = ""; 
+    this.content = "";
+    this.bibtex = "";
 
-    
+    this.getTitle = function getTitle(){
+        return this.title;
+    }
+    this.setTitle = function setTitle(arg){
+        this.title = arg;
+    }
+    this.getAuthors = function getAuthors(){
+        return this.authors;
+    }
+    this.setAuthors = function setAuthors(arg){
+         this.authors = arg;
+    }
+    this.getConference = function getConference(){
+        return this.conference;
+    }
+    this.setConference = function setConference(arg){
+        this.conference = arg;
+    }
+    this.getContent = function getContent(){
+        return this.content;
+    }
+    this.setContent = function setContent(arg){
+         this.content = arg;
+    }
+    this.getAbstract = function getAbstract(){
+        return this.abstract;
+    }
+    this.setAbstract = function setAbstract(arg){
+        this.abstract;
+    }
+    this.setLink = function setLink(arg){
+        this.link = arg;
+    }
+    this.getLink = function getLink(){
+        return this.link;
+    }
+    this.setBibtex = function setBibtex(arg){
+        this.bibtex = arg;
+    }
+    this.getBibtex = function getBibtex(){
+        return this.bibtex;
+    }
 
+}
+
+
+
+
+
+
+var td = new Array();
+var tr = new Array();
+var tempPapers = new Array();
+var tempfrequencies = new Array();
+
+    for (i = 0; i <10; i++) {
+
+
+        //tempfrequencies [i] = Math.random();
+        tempPapers[i] = new Paper();
+        tempPapers[i].setTitle("paper" + i );
+        tempPapers[i].setAuthors("Author" + i );
+        tempPapers[i].setConference("Conference" + i );
+        tempPapers[i].setLink("https://www.youtube.com/");
+        tempPapers[i].setBibtex("Bibtext" + i );
+
+}
+
+   var myTable = document.getElementById("paperListTable");
+         for (i=0; i < 10; i++) { 
+            var tr = document.createElement('TR');
+            
+            td.push(Math.floor(Math.random() * 15) + 1 );
+            td.push(tempPapers[i].getTitle());
+            td.push(tempPapers[i].getAuthors());
+            td.push(tempPapers[i].getConference());
+            td.push(tempPapers[i].getLink());
+            td.push(tempPapers[i].getBibtex());
+
+
+
+
+                for(p = 0; p<td.length; p++){
+                tdd = document.createElement('TD');
+                tdd.appendChild(document.createTextNode(td[p]));
+                tr.appendChild(tdd);
+
+                
+            }
+
+                
+            td = [];
+            myTable.appendChild(tr);
+            
+
+
+}
+
+    </script>
 
     </table>
    
@@ -81,9 +176,10 @@ $frequencies = [];
     <button id = "downloadAsTXT" onClick="HTMLtoTXT()"> Download List (TXT)</button>
     <button id = "backToCloud"> Back to Cloud Page</button>
     <br>
-    <button id = "RunIt" onclick="RunProgram()"> Run Program</button>
+    <button id = "RunIt" onClick="RunProgram()"> Run Program</button>
     <br>
     <button id = "fillUpData" onClick = "FillDataUp()"> Fill Up Data </button>
+
 
 
 </body>
@@ -94,16 +190,10 @@ $frequencies = [];
 <script type = "text/javascript" src="../tableExport.jquery.plugin-master/jquery.base64.js"></script>
 <script type = "text/javascript" src="../tableExport.jquery.plugin-master/tableExport.js"></script>
 <script type = "text/javascript" src="../tableExport.jquery.plugin-master/html2canvas.js"></script>
-<script type="text/javascript">
-        $(document).ready(function(){
-$("downloadAsTXT").click(function(){
-    $("paperListTable").tableExport({
-        type:'txt',
-        escape:'false',
-    });
-});
-}); 
+
+
 </script>
+
 <script>
 
 /* <!-- 
@@ -116,27 +206,16 @@ var pageContent = "";
 var pageAuthors = [];
 var pageBibtext = "";
 var pageLink = "";  
-/* class Paper {
-    
-} */
 
-/*
-function HTMLtoTXT() {
-    $("paperListTable").tableExport({
-        type:'txt',
-        escape: 'false',
-    }); 
- 
-}
 
-*/
+
 var fileName =  'tags.html';
 
-/*function HTMLtoTXT(){
+function HTMLtoTXT(){
    
     downloadInnerHtml(fileName, 'HTMLtoPDF','text/html');
 
-}*/
+}
 
 
 function  RunProgram(){
@@ -145,155 +224,46 @@ function  RunProgram(){
 function FillDataUp(){
     
 }
-/*function demoFromHTML() {
-    var pdf = new jsPDF('p', 'pt', 'letter');
-
-    pdf.cellInitialize();
-    pdf.setFontSize(10);
-    $.each( $('#paperListTable tr'), function (i, row){
-        $.each( $(row).find("td, th"), function(j, cell){
-            var txt = $(cell).text().trim() || " ";
-            var width = (j==4) ? 40 : 70; //make 4th column smaller
-            pdf.cell(10, 50, width, 30, txt, i);
-        });
-    });
-
-    pdf.save('sample-file.pdf');
-}*/
 
 </script>
 
 
 </html>
 
+<!--         <?php for ($i=1; $i <= 10; $i++) { ?>
+            <tr> 
+                <td> <?php echo $frequencies[$i]?> </td>
+                <td> <?php echo $papers[$i]->getTitle();?> </td>
+                <td> <?php echo $papers[$i]->getAuthors(); ?> </td>
+                <td> <?php echo $papers[$i]->getConference();?> </td>
+                <td> <a href=" https://www.youtube.com/">  <?php echo $papers[$i]->getLink();?></a> </td>
+                <td> <?php echo $papers[$i]->getBibtex(); ?></td>
 
-<!-- 
-<html>
-    <head>
-        <title>IEEE Cloud</title>
-        <link rel="stylesheet" type="text/css" href="styles.css">
-        <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
-        <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
-        <script src="scripts/helper.js"></script>
-        <script src="scripts/postImageToFacebook.js"></script>
-        <script src="https://d3js.org/d3.v4.min.js"></script>
-        <script src="scripts/d3.layout.cloud.js"></script>
-        <script type="text/javascript" src="http://canvg.github.io/canvg/rgbcolor.js"></script> 
-        <script type="text/javascript" src="http://canvg.github.io/canvg/StackBlur.js"></script>
-        <script type="text/javascript" src="http://canvg.github.io/canvg/canvg.js"></script> 
-        <script>
-                    
-        </script>
-    </head>
-    <body>
-    <div id="songListPage">
+            </tr>
+         <?php } ?> -->
 
-    <a id="backToWordCloud">Back to Word Cloud</a>
-    
-    <h1 id="songListTitle"  >Song List Page</h1>
-    
-    <ul id="songList"></ul>
-    
-</div>
+<!-- // /*function HTMLtoTXT() {
 
-<style>
+//         <?php 
+//        $s = "hey" ;
+//        for ($i=1; $i <= 10; $i++) { 
+//         /*$s = "hi";*/
+             
+//                $string .=   $frequencies[$i];
+//                $string .=  $papers[$i]->getTitle(); 
+//                $string .=  $papers[$i]->getAuthors();  
+//                $string .= $papers[$i]->getConference();
+//                $string .=  "https://www.youtube.com/" ; 
+//                $string .=$papers[$i]->getBibtex(); 
 
-#songListPage {
-    
-    width: 75%;
-    margin: auto;
-    text-align: center;
-    
-    margin-top: 25px;
-}
-
-#songListTitle {
-    margin-top: 25px;
-}
-
-#songList {
-    line-height: 200%;
-    /*
-    background-color: white;
-    border: 2px black solid;
-    width: 800px;
-    */
-    margin: auto;
-    padding: 10px;
-}
-
-/*
-div.relative {
-    position: relative;
-    width: 40px;
-    height: 20px;
-    border: 3px solid #73AD21;
-}
-h1 {
-    text-align: center;
-    left: 0;
-    line-height: 200px;
-    margin: auto;
-    margin-top: -100px;
-    position: absolute;
-    top: 30%;
-    width: 100%;
-}
-.left {
-    text-align: center;
-    left: 650;
-    line-height: 200px;
-    margin: auto;
-    margin-top: -100px;
-    position: absolute;
-    top: 20%;
-}
-*/
-
-</style> -->
-
-<!-- <script>
-
-    // Set the title (of document and header)
-    var title = word.charAt(0).toUpperCase() + word.substring(1);
-    document.title = title;
-    $("#songListTitle").text(title);
-    
-    // Set up Song List
-    
-    $.each(songs, function(index, song) { // count the word in each song
-        
-        // Count the occurances of the word
-        var wordMatches = song.lyrics.match(regexForWord(word));
-        song.wordCount = 0;
-        if (wordMatches != null) {
-            song.wordCount = wordMatches.length;
-        }
-    })
-    
-    songs.sort(function(a, b) { // sort songs by word count, descending
-        return b.wordCount - a.wordCount;
-    });
-    
-    $.each(songs, function(index, song) { // add each song to the list
-        if (song.wordCount > 0) {
-            $("#songList").append("<li><a class='songListLink' value='" + index + "'>" + song.title + "</a> by " + song.artist + " (" + song.wordCount + " occurances)</li>");
-        }
-    })
-    
-    // set up links
-    
-    $(".songListLink").click(function() {
-        song = songs[$(this).attr("value")];
             
-        loadPage("lyrics");
-    });
-    
-        
-    // Set up back button
-    $("#backToWordCloud").click(function() {
-        loadPage("wordCloud");
-    });
-
-</script>
- -->
+//          }
+//          header("Content-Disposition: attatchment; filename = temp.txt"); 
+//          header("Content-Type: text/plain");
+         
+//          echo $s; ?>
+// //                  /*    $("paperListTable").tableExport({
+//         type:'txt',
+//         escape: 'false',
+//     }); 
+//  */ -->
